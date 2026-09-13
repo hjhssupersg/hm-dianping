62cea07aacd588e94d653b7d3ae1751e391a1604
hjhssupersg <3330288532@qq.com>
2026-09-13 14:14:53 +0800

    完成了“根据id更新商铺信息”的接口设计

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -10,6 +10,7 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.utils.RedisConstants;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.util.concurrent.TimeUnit;
@@ -36,23 +37,51 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
     @Override
     public Result queryById(Long id) {
         String key = RedisConstants.CACHE_SHOP_KEY + id;
-        //根据id从redis中查询店铺信息
+        //根据id从redis中查询商铺信息
         String shopJson = stringRedisTemplate.opsForValue().get(key);
         //若存在，直接返回
         if (StrUtil.isNotBlank(shopJson)) {
             Shop shop = JSONUtil.toBean(shopJson, Shop.class);
             return Result.ok(shop);
         }
+        //若为空值，返回错误信息
+        if (shopJson != null) {
+            return Result.fail("店铺信息不存在！");
+        }
         //若不存在，从数据库查询
         Shop shop = getById(id);
         //不存在，报错
         if (shop == null) {
+            //将空值写入redis，防止缓存穿透
+            stringRedisTemplate.opsForValue().set(key, "",
+                    RedisConstants.CACHE_NULL_TTL, TimeUnit.MINUTES);
             return Result.fail("店铺不存在！");
         }
-        //存在，将查询到的店铺信息写入redis并设置过期时间
+        //存在，将查询到的商铺信息写入redis并设置过期时间
         stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shop),
                 RedisConstants.CACHE_SHOP_TTL, TimeUnit.MINUTES);
         //返回
+        return Result.ok(shop);
+    }
+
+    /**
+     * 根据id更新商铺信息
+     * @param shop
+     * @return
+     */
+    @Override
+    @Transactional
+    public Result update(Shop shop) {
+        //先判断商铺id是否为空
+        Long id = shop.getId();
+        if (id == null) {
+            //若为空，报错
+            return Result.fail("商铺id不能为空！");
+        }
+        //根据id在数据库更新商铺信息
+        updateById(shop);
+        //删除redis中的商铺信息
+        stringRedisTemplate.delete(RedisConstants.CACHE_SHOP_KEY + shop.getId());
         return Result.ok(shop);
     }
 }

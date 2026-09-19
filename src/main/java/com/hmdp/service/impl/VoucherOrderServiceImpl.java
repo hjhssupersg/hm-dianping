31553ccec959bee87e656d5b0e5baaec910b268c
hjhssupersg <3330288532@qq.com>
2026-09-19 11:48:44 +0800

    解决了用户下单优惠券时的超卖问题（保证stock>0时才更新数据）

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -53,7 +53,10 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
         }
         //减少库存
         boolean success = seckillVoucherService.update().
-                setSql("stock = stock - 1").eq("voucher_id", voucherId).update();
+                setSql("stock = stock - 1").//set stock = stock - 1
+                eq("voucher_id", voucherId).
+                gt("stock", 0).//where voucher_id = ? and stock > 0
+                update();
         if (!success) {
             return Result.fail("库存不足！");
         }
